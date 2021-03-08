70cccb3eb85cc2fc1d849cc86ae0b21b20afbce6
hgs-study <hgstudy_@naver.com>
2021-03-08 23:44:44 +0900

    create jpa-shop project

---
+++ src/main/java/domain/Item.java
@@ -0,0 +1,48 @@
+package domain;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Item {
+
+    @Id @GeneratedValue
+    @Column(name= "ITEM_ID")
+    private Long Id;
+    private String name;
+    private int price;
+    private int stockQuantity;
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getStockQuantity() {
+        return stockQuantity;
+    }
+
+    public void setStockQuantity(int stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
+}
---
+++ src/main/java/domain/Member.java
@@ -0,0 +1,54 @@
+package domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Member {
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="MEMBER_ID")
+    private Long id;
+    private String name;
+    private String city;
+    private String street;
+    private String zipCode;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+}
