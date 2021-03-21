1d5a101eff6c7647369dd02099108e9903b09288
hgs-study <hgstudy_@naver.com>
2021-03-21 17:06:23 +0900

    [연관관계 매핑] 양방향 VS 단방향

--- src/main/java/domain/Member.java
+++ src/main/java/domain/Member.java
@@ -14,8 +14,11 @@ public class Member {
     private String street;
     private String zipCode;
 
+    //김영한님은 거의 이걸 잘못된 코드라고 생각한다. 테스트이기 때문에 이렇게 사용함
+    //Member에 굳이 orders 양방향으로 없어도 되기 때문에 (Members에서 Orders를 꺼낼 일이 거의 없기 때문에)
     @OneToMany(mappedBy = "member")
     private List<Order> orders = new ArrayList<>();
+
     public Long getId() {
         return id;
     }
