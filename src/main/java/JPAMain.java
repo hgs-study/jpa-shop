c3fd387e6f443b956ed899a210e4fa949ef1ac35
hgs-study <hgstudy_@naver.com>
2021-03-21 17:02:26 +0900

    [JPA] 양방향 VS 단방향

--- src/main/java/JPAMain.java
+++ src/main/java/JPAMain.java
@@ -16,8 +16,22 @@ public class JPAMain {
 
         tx.begin();
         try{
+            //방법 1 (양방향 연관관계)
+            //Order order = new Order();
+            //order.addOrderItem(new OrderItem());
+
+
+            //방법 2 (단방향 연관관계)
+            //단방향 연관관계로도 애플리케이션을 만드는데에 지장이 없다.
+            //양방향 연관관계로 하는 이유는 조회를 더 편하게 하기 위해서
+            //나중에 JPQL로 조인을 더 복잡하게 해야하는데 이 작업을 더 간단히 하기위해 양방향으로 많이 짠다.
+            //핵심은 단방향으로 할 수 있으면 최대한 단방향으로 해라!
             Order order = new Order();
-            order.addOrderItem(new OrderItem());
+            em.persist(order);
+
+            OrderItem orderItem = new OrderItem();
+            orderItem.setOrder(order);
+            em.persist(orderItem);
 
 
             tx.commit();
