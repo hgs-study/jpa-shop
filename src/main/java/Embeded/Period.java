bf5b852b5c2927d5efeda275bfa68dd1a3f7186d
hgs-study <hgstudy_@naver.com>
2021-03-30 21:18:01 +0900

    [불변객체] 임베디드 타입을 Setter를 지움 / Setter 접근 제한자를 private으로 변경함으로써 불변객체를 만들고 side effect를 없앤다.

--- src/main/java/Embeded/Period.java
+++ src/main/java/Embeded/Period.java
@@ -16,15 +16,15 @@ public class Period {
         return startDate;
     }
 
-//    public void setStartDate(LocalDateTime startDate) {
-//        this.startDate = startDate;
-//    }
+    private void setStartDate(LocalDateTime startDate) {
+        this.startDate = startDate;
+    }
 
     public LocalDateTime getEndDate() {
         return endDate;
     }
 
-//    public void setEndDate(LocalDateTime endDate) {
-//        this.endDate = endDate;
-//    }
+    private void setEndDate(LocalDateTime endDate) {
+        this.endDate = endDate;
+    }
 }
